a02a610c97b2bd1cc9e503856e62bc47b12cff31
fafeoik <yes-99@mail.ru>
2024-06-20 14:43:20 +0700

    Product DTO name changed

--- ProductsAPI/DTO/ProductUpdateDTO.cs
+++ ProductsAPI/DTO/ProductUpdateDTO.cs
@@ -1,8 +1,13 @@
-﻿namespace ProductsApi.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductsApi.DTO
 {
     public class ProductUpdateDTO
     {
+        [Required]
         public string? Name { get; set; }
+        [Required]
+        [Range(0, int.MaxValue, ErrorMessage = "Цена должна быть больше 0.")]
         public int Price { get; set; }
     }
 }
